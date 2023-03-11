2da6cec152d644d8de6cebb8b17f95ae9c2475c7
KiritiAryal <91573969+KiritiAryal@users.noreply.github.com>
2023-03-10 23:42:37 -0600

    Create assignment_3.sh

---
+++ assignment_3.sh
@@ -0,0 +1,8 @@
+module load gnu/5.4.0
+touch a3_results.txt
+echo Kiriti Aryal > a3_results.txt 
+echo R11722758 >> a3_results.txt
+cd code/decrypt
+./decrypt /lustre/work/errees/courses/cs4352/assignment3/input/encrypted.txt
+../transform/transform 11722758
+sed -n '5{p;q}' /lustre/work/errees/courses/cs4352/assignment3/input/.hidden_file >> ../../a3_results.txt 
