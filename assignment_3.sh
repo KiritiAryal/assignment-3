e59c75116d551da99045a44d17c7265f25fbb4ef
KiritiAryal <91573969+KiritiAryal@users.noreply.github.com>
2023-03-11 00:11:35 -0600

    Update assignment_3.sh

--- assignment_3.sh
+++ assignment_3.sh
@@ -4,6 +4,6 @@ echo Arogya Bhatta > a3_results.txt
 quanah:/work3$ echo R11594739>>a3_results.txt
 cd code/decrypt
 ./decrypt /lustre/work/errees/courses/cs4352/assignment3/input/encrypted.txt
-../transform/transform 11722758
-sed -n '5{p;q}' /lustre/work/errees/courses/cs4352/assignment3/input/.hidden_file >> ../../a3_results.txt 
+../transform/transform 11594739
+sed -n '57{p;q}' /lustre/work/errees/courses/cs4352/assignment3/input/.hidden_file >> ../../a3_results.txt 
 cd ~
